7fe4d58caaf97e67ab28b24070dba81e5a7c25b1
Bruno Rodrigues <brodrigues1990@gmail.com>
2020-01-15 05:20:28 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Header></Header>
+      <SearchBar></SearchBar>
+      <SimpleList></SimpleList>
+      <Footer></Footer>
+    </div>
+
+  );
+}
+
+export default App;
