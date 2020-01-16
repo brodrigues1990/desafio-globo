edf1896bd95bce416de05a468916d09f5140e620
Bruno <brodrigues1990@gmail.com>
2020-01-16 06:01:47 -0300

    Criacao da header

---
+++ src/components/organisms/Header/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Container, Row, Column } from '../../atoms/Grid/Grid'
+
+const HeaderContainer = styled.header `
+    width:100%;
+    height: 72px;
+    background-color: #465A65;
+    color: #FFF;
+    position: fixed;
+    top: 0;
+    transform: translateY(-100%);
+    transition: transform .3s cubic-bezier(.4,0,.6,1),opacity 0s .3s;
+    box-shadow: 0 2px 4px rgba(0,0,0,.5);
+    z-index: 4;
+    opacity: 1;
+    transform: translateY(0);
+    transition: transform .3s cubic-bezier(.4,0,.2,1) .3s,opacity 0s .3s;
+`
+
+export default class Header extends Component {
+  render() {
+    return (
+        <HeaderContainer>
+        
+        </HeaderContainer>
+    );
+  }
+}
