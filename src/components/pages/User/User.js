b119511d422dca48a8d935753fa0e69368717911
Bruno <brodrigues990@gmail.com>
2020-01-20 11:19:57 -0300

    Pequenos ajustes de layout na pagina User

--- src/components/pages/User/User.js
+++ src/components/pages/User/User.js
@@ -1,14 +1,55 @@
 import React, { Fragment } from 'react';
 import Header from '../../organisms/Header/Header';
 import styled from 'styled-components';
-import { Container } from '../../atoms/Grid/Grid';
+import { Row, Column } from '../../atoms/Grid/Grid';
 import Card from '../../molecules/Card/Card';
-import githubAPI, { api } from '../../../services/githubAPI';
+import useGetApi, { api } from '../../../services/githubAPI';
 import axios from 'axios';
+import { ListItem } from '../../molecules/SimpleList/SimpleList';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const ImageUser = styled.img`
-     width: 100px;
+     width: 200px;
 `
+const Container = styled.div`
+  padding: 25px 35px 15px 35px;
+`
+const ColumnUserInfo = styled(Column)`
+  flex:3;
+`
+const ListRepos = styled(ListItem)`
+  flex-direction: column;
+  -webkit-align-items: start;
+  -webkit-box-align: start;
+  -ms-flex-align: center;
+  align-items: start;
+`
+
+const ContainerText = styled.div`
+    margin-bottom: 5px;
+`
+const Text = styled.span`
+    color: #465A65;
+    font-weight: 300;
+    height:15px;
+`
+const Text1 = styled(Text)`
+    font-size: 25px;
+    font-weight: 400;
+    margin-bottom: 15px; 
+`
+const Text2 = styled(Text)`
+    font-size: 21px;
+`
+const Text3 = styled(Text)`
+    font-size: 17px;
+`
+const Text4 = styled(Text)`
+    font-size: 15px;
+`
+
+
 
 export default class User extends React.Component {
   constructor(props) {
@@ -24,8 +65,9 @@ export default class User extends React.Component {
   }
 
   componentDidMount() {
-    // const users = githubAPI('user', props.match.params.id);
+    // const users = useGetApi('user', this.state.userLogin);
     // console.log(users);
+
     axios.get(api.baseUrl + "/users/" + this.state.userLogin)
       .then(res => {
         // console.log(res.data);
@@ -58,45 +100,49 @@ export default class User extends React.Component {
       <Fragment>
 
         <Card title={this.state.userLogin}>
-          <ImageUser src={this.state.userInfo.avatar_url} />
 
-          nome:{this.state.userInfo.name}<br />
-          bio:{this.state.userInfo.bio}<br />
-          localidade{this.state.userInfo.location}<br />
+          <Container>
+            <Row>
+              <Column>
+                <ImageUser src={this.state.userInfo.avatar_url} />
+              </Column>
+              <ColumnUserInfo>
+                <ContainerText><Text1>{this.state.userInfo.name}</Text1></ContainerText>
+                <ContainerText><Text2>{this.state.userInfo.bio}</Text2></ContainerText>
+                <ContainerText><Text4><FontAwesomeIcon icon={faMapMarkerAlt} size="lg" /> {this.state.userInfo.location}</Text4></ContainerText>
+              </ColumnUserInfo>
+            </Row>
+          </Container>
         </Card>
 
-        <Card title="Repositório">
+        <Card title="Repositórios">
+          <Container>
+            <div><Text3>Tamanho total:&nbsp;</Text3><Text2>{this.state.repoTotalSize}</Text2><Text3>&nbsp;Bytes</Text3></div>
+            <div><Text3>Issues abertos:&nbsp;</Text3><Text2>{this.state.repoTotalIssues} </Text2></div>
+            <div><Text3>Linguagens:&nbsp;</Text3><Text2></Text2></div>
+          </Container>
 
-          Tamanho total(soma):{this.state.repoTotalSize}  <br />
-          Linguagens   <br />
-          Issues abertos:  {this.state.repoTotalIssues}  <br /><br />
-          <ul>
           {
             this.state.UserRepos.map((repos, i) => {
               return (
 
-                
-                  <li key={i}>
-                    nome do repositorio: {repos.name} <br />
-                    descricao : {repos.name} <br />
-                    tamanho:  {repos.size} <br />
-                    linguagem:  {repos.language} <br />
-                    issues abertos:  {repos.open_issues_count} <br /><br /><br />
-                  </li>
-                
+                <ListRepos key={i} >
+                  <div><Text2>{repos.name} </Text2></div>
+                  <div><Text3>descricao:&nbsp;</Text3><Text3>{repos.name}</Text3></div>
+                  <div><Text3> tamanho:&nbsp;</Text3><Text3>{repos.size}</Text3></div>
+                  <div><Text3>linguagem:&nbsp;</Text3><Text3>{repos.language}</Text3></div>
+                  <div><Text3>issues abertos:&nbsp;</Text3><Text3>{repos.open_issues_count}</Text3></div>
+                </ListRepos>
+
+
 
               )
             })
           }
-          </ul>
         </Card>
-
-
-
       </Fragment>
     );
   }
 
 }
-
 
