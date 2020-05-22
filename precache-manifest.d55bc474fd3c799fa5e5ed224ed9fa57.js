862696aedcd828ccd1d840306f81e09688e5b084
Bruno Rodrigues <brodrigues1990@gmail.com>
2020-05-22 10:31:58 -0300

    Updates

---
+++ precache-manifest.d55bc474fd3c799fa5e5ed224ed9fa57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49055bc75e2eea7c05cf09daffa0ecf",
+    "url": "/desafio-globo/index.html"
+  },
+  {
+    "revision": "0aa4462731b9c799e0ab",
+    "url": "/desafio-globo/static/css/main.04bc1773.chunk.css"
+  },
+  {
+    "revision": "ca30906636e5f6186249",
+    "url": "/desafio-globo/static/js/2.71af5dec.chunk.js"
+  },
+  {
+    "revision": "8e8e359918da60444c0f7f853701318b",
+    "url": "/desafio-globo/static/js/2.71af5dec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0aa4462731b9c799e0ab",
+    "url": "/desafio-globo/static/js/main.a535d6b8.chunk.js"
+  },
+  {
+    "revision": "0dc2058402bfcf1a3a2c",
+    "url": "/desafio-globo/static/js/runtime-main.686cd6fe.js"
+  },
+  {
+    "revision": "89c2c0674bdba3d61daf6b1b1e5e82b5",
+    "url": "/desafio-globo/static/media/logo-github.89c2c067.png"
+  }
+]);
